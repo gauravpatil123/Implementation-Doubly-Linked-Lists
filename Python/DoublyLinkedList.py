975f9ef092ce63dafa86c3ff354ea312fb83e917
gauravpatil123 <gaurav.patil123@gmail.com>
2020-06-12 18:42:26 +0530

    added doc strings to DoublyLinkedList.py

--- Python/DoublyLinkedList.py
+++ Python/DoublyLinkedList.py
@@ -14,14 +14,24 @@ class DoublyLinkedList:
 
     # true if list is empty else false
     def isEmpty(self):
+        """
+        return True if the LL is empty else returns False
+        """
         return self.num == 0
 
     # returns number of elements
     def size(self):
+        """
+        return the integer size of the list
+        """
         return self.num
 
     # prepends element to the collection
     def addFirst(self, item):
+        """
+        Input: item
+        Action: adds item to first position (0 index) of the list
+        """
         if (item == None): 
             raise Exception("Illegal argument")
 
@@ -34,6 +44,10 @@ class DoublyLinkedList:
 
     # appends element to the collection
     def addLast(self, item):
+        """
+        Input: item
+        Action: adds item to the last position of the list
+        """
         if (item == None):
             raise Exception("Illegal argument")
 
@@ -46,6 +60,11 @@ class DoublyLinkedList:
 
     # Inserts element at specified position
     def add(self, index, item):
+        """
+        Inputs: an integer index, item
+        Action: adds the item to the specified index in the list if the
+                index in not out of bounds
+        """
         if (index >= self.num or index < 0):
             raise Exception("Index out of bounds")
         if (item == None):
@@ -66,6 +85,9 @@ class DoublyLinkedList:
 
     # removes first element of the list
     def removeFirst(self):
+        """
+        removes and returns the first element of the list (index = 0)
+        """
         if (self.num == 0):
             raise Exception("List is empty")
 
@@ -78,6 +100,9 @@ class DoublyLinkedList:
 
     # removes last element of the list
     def removeLast(self):
+        """
+        removes and return the last element of the list
+        """
         if (self.num == 0):
             raise Exception("List is empty")
 
@@ -90,6 +115,9 @@ class DoublyLinkedList:
 
     # clears all elements of list
     def clear(self):
+        """
+        clears all the elements from the list
+        """
         self.first = Node(None)
         self.last = Node(None)
         self.num = 0
@@ -98,6 +126,9 @@ class DoublyLinkedList:
 
     # creates and return identical list
     def clone(self):
+        """
+        creates and returns a new identical linked list
+        """
         newLL = DoublyLinkedList()
         count = 0
         current = Node(None)
@@ -113,6 +144,11 @@ class DoublyLinkedList:
 
     # checks if the list has the specified element
     def contains(self, item):
+        """
+        Input: item
+        Output: returns True if the Linked list has the input item
+                else returns False
+        """
         if (item == None):
             raise Exception("Illegal argument")
 
@@ -130,12 +166,23 @@ class DoublyLinkedList:
     
     # returns first element of the list without removing it
     def element(self):
+        """
+        return the first element of the list (index = 0)
+        """
         if (self.num == 0):
             raise Exception("List is empty")
         return self.first.next.item
 
     # returns element at a specified position
     def get(self, index):
+        """
+        Input: an integer index
+        Output: return the item fromt he list at the input index
+                if the index is not out of bounds
+        """
+        if (index >= self.num):
+            raise Exception("index out of bounds")
+
         idx = 0
         current = self.first.next
 
@@ -148,18 +195,29 @@ class DoublyLinkedList:
 
     # returns first element
     def getFirst(self):
+        """
+        returns the first element of list without mutation (index = 0)
+        """
         if (self.num == 0):
             raise Exception("List is empty")
         return self.first.next.item
 
     # returns last element
     def getLast(self):
+        """
+        returns the last element of the list without mutation
+        """
          if (self.num == 0):
              raise Exception("List is empty")
          return self.last.prev.item
 
     # returns the index of first occurence of specified element or -1 if no such element
     def indexOf(self, item):
+        """
+        Input: item
+        Output: returns the index of the first occurence of the input item or
+                -1 if no such item is present in the list
+        """
         if (self.num == 0):
             return -1
         idx = 0
@@ -176,6 +234,9 @@ class DoublyLinkedList:
 
     # iterates the list
     def iterate(self):
+        """
+        yields elements of the list according to index order
+        """
         if (self.num == 0):
             raise Exception("List is empty")
         current = Node(None)
@@ -187,6 +248,9 @@ class DoublyLinkedList:
 
     # returns a python list
     def toList(self):
+        """
+        return a python list identical to the linked list
+        """
         if (self.num == 0):
             raise Exception("List is empty")
         out = []
